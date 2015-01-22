08d328c364ca844cf20dca71f5b8df45afa964e7
pkuwalter <pkuwalter@qq.com>
2015-01-22 18:31:57 +0800

    Signed-off-by: pkuwalter <pkuwalter@qq.com>

---
+++ DataScience/02_RProgramming/Assessment2/cachematrix.R
@@ -0,0 +1,33 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## Write a short comment describing this function
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(Y) {
+    x <<- Y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  inv <- X$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- X$get()
+  inv <- solve(data, ...)
+  X$setinverse(inv)
+  inv
+}
